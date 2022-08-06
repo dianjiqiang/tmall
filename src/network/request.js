4fd23fbd76d4189f50b0ead1aafe43154a587208
609661157@qq.com <609661157@qq.com>
2022-08-06 21:09:24 +0800

    维护

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 export function request(config) {
   const instance = axios.create({
-    baseURL:'http://152.136.185.210:7878/api/m5',
+    baseURL:'http://152.136.185.210:7878/api/hy66',
     timeout:5000
   })
   instance.interceptors.response.use(res=>{				//拦截响应
